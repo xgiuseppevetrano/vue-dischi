58485414baebcf02277016ff560fd1a1d2ec1049
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-06-07 16:23:32 +0200

    add scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BaseHeader/>
+    <BaseMain/>
+    <BaseFooter/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseHeader from './components/blocks/BaseHeader.vue'
+import BaseMain from './components/blocks/BaseMain.vue'
+import BaseFooter from './components/blocks/BaseFooter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BaseHeader,
+    BaseMain,
+    BaseFooter,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './assets/style/global.scss';
 </style>
